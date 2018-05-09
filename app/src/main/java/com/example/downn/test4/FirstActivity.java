e940d837ea304274843888d3484087bcf3fb30d9
Downnnn <836436151@qq.com>
2018-05-09 18:08:11 +0800

    Initial commit

---
+++ app/src/main/java/com/example/downn/test4/FirstActivity.java
@@ -0,0 +1,26 @@
+package com.example.downn.test4;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("com.example.downn.test4.ACTION_START");
+                intent.addCategory("com.example.downn.test4.MY_CATEGORY");
+                startActivity(intent);
+            }
+        });
+    }
+}
